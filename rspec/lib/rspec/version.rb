8468e14731bbd02612f642ad821f5483acab7195
Koma <koma@ubuntu.(none)>
2012-06-03 00:27:20 -0600

    part 5

---
+++ rspec/lib/rspec/version.rb
@@ -0,0 +1,3 @@
+module Rspec
+  VERSION = "0.0.1"
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,16 +4,22 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Sample App')
     end
-    it "should have the right title" do
+
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "Ruby on Rails Tutorial Sample App")
     end
-  end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => '| Home')
+    end
+  end 
 
   describe "Help page" do
 
@@ -38,6 +44,20 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_selector('title',
         :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
